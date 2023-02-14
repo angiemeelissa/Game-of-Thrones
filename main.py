83590197df7f9e2f1a57ad7bfdeb29db239e68b0
Angie Melissa <124700371+angiemeelissa@users.noreply.github.com>
2023-02-14 09:56:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print("Angie Melissa Santiago Rodriguez - 1555123")
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
